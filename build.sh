d8d20e7abaf6c5fb5002dbaa6ed4a27d85b7ef97
Dheeraz Satsangi <satsangi.dheeraz@gmail.com>
2022-12-05 12:10:39 +0530

    debug 2

--- build.sh
+++ build.sh
@@ -5,7 +5,9 @@ echo "Docker Container Building $1:$2"
 set -x -e
 
 ls -all
+rm -rf $1/dist
+rm -rf $1/build
 
-#/opt/mplabx/mplab_platform/bin/prjMakefilesGenerator.sh $1@$2 || exit 1
-#make -C $1 CONF=$2 build || exit 2
+/opt/mplabx/mplab_platform/bin/prjMakefilesGenerator.sh $1@$2 || exit 1
+make -C $1 CONF=$2 build || exit 2
 
