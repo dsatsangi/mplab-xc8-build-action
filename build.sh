ed6efcbad9e14f4cae3a3f81a33c21b907e7762c
Dheeraz Satsangi <satsangi.dheeraz@gmail.com>
2022-12-05 22:24:57 +0530

    added some more

--- build.sh
+++ build.sh
@@ -17,4 +17,6 @@ rm -rf $1/build
 #ls $1 -all
 
 /opt/mplabx/mplab_platform/bin/prjMakefilesGenerator.sh $1@$2 || exit 1
-make -C $1 CONF=$2 build || exit 2
+#make -C $1 CONF=$2 build || exit 2
+
+build-wrapper-linux-x86-64 --out-dir ${{ env.BUILD_WRAPPER_OUT_DIR }} make -C $1 CONF=$2 build || exit 2
