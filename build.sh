f23fcd194e222eba32d36531a09c0ea1d8852270
Dheeraz Satsangi <satsangi.dheeraz@gmail.com>
2022-12-05 22:39:02 +0530

    new action.yml

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ rm -rf $1/build
 #rm -rf $1/build/nbproject !(*.xml)
 #ls $1 -all
 
-/opt/mplabx/mplab_platform/bin/prjMakefilesGenerator.sh $1@$2 || exit 1
+#/opt/mplabx/mplab_platform/bin/prjMakefilesGenerator.sh $1@$2 || exit 1
 #make -C $1 CONF=$2 build || exit 2
 
-build-wrapper-linux-x86-64 --out-dir ${{ env.BUILD_WRAPPER_OUT_DIR }} make -C $1 CONF=$2 build || exit 2
+#build-wrapper-linux-x86-64 --out-dir ${{ env.BUILD_WRAPPER_OUT_DIR }} make -C $1 CONF=$2 build || exit 2
