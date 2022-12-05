44eea93ed203280a8291a0a15526a641a88a57a9
Dheeraz Satsangi <satsangi.dheeraz@gmail.com>
2022-12-05 13:07:28 +0530

    debug 4

--- build.sh
+++ build.sh
@@ -4,10 +4,16 @@ echo "Docker Container Building $1:$2"
 
 set -x -e
 
-ls $1 -all
+#ls $1 -all
 rm -rf $1/dist
 rm -rf $1/build
-rm -v $1/build/nbproject !(*.xml)
+rm -v $1/nbproject/Makefile-impl.mk
+rm -v $1/nbproject/Makefile-default.mk
+rm -v $1/nbproject/Makefile-genesis.mk
+rm -v $1/nbproject/Makefile-local-default.mk
+rm -v $1/nbproject/Makefile-variables.mk
+rm -v $1/nbproject/Package-default.bash
+#rm -v $1/build/nbproject !(*.xml)
 #ls $1 -all
 
 /opt/mplabx/mplab_platform/bin/prjMakefilesGenerator.sh $1@$2 || exit 1
