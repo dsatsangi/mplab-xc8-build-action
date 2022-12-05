bebc12e540ec4825a7162b1add4d73b72812324a
Dheeraz Satsangi <satsangi.dheeraz@gmail.com>
2022-12-05 13:16:09 +0530

    debug 9

--- build.sh
+++ build.sh
@@ -7,13 +7,13 @@ set -x -e
 #ls $1 -all
 rm -rf $1/dist
 rm -rf $1/build
-rm -v $1/nbproject/Makefile-impl.mk
-rm -v $1/nbproject/Makefile-default.mk
-rm -v $1/nbproject/Makefile-genesis.mk
-rm -v $1/nbproject/Makefile-local-default.mk
-rm -v $1/nbproject/Makefile-variables.mk
-rm -v $1/nbproject/Package-default.bash
-#rm -v $1/build/nbproject !(*.xml)
+rm -rf $1/nbproject/Makefile-impl.mk
+rm -rf $1/nbproject/Makefile-default.mk
+rm -rf $1/nbproject/Makefile-genesis.mk
+rm -rf $1/nbproject/Makefile-local-default.mk
+rm -rf $1/nbproject/Makefile-rfariables.mk
+rm -rf $1/nbproject/Package-default.bash
+#rm -rf $1/build/nbproject !(*.xml)
 #ls $1 -all
 
 /opt/mplabx/mplab_platform/bin/prjMakefilesGenerator.sh $1@$2 || exit 1
