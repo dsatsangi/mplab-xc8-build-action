11c9a55708ccd56ac5e219b03b22a581d4bad9c9
Dheeraz Satsangi <satsangi.dheeraz@gmail.com>
2022-12-07 15:36:53 +0530

    build canged

--- build.sh
+++ build.sh
@@ -1,50 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 
-# use only one of SonarQube or SonarCloud
+echo "Docker Container Building $1:$2"
 
-# Configuration for SonarQube
-#SONAR_HOST_URL="http://localhost:9000" # URL of the SonarQube server
-#SONAR_TOKEN=f5f56032a938d29cf76d78de33991eb8c273a0ea # access token from SonarQube projet creation page -Dsonar.login=XXXX
-#SONAR_PROJECT_KEY=sonar_scanner_example # project name from SonarQube projet creation page -Dsonar.projectKey=XXXX
-#SONAR_PROJECT_NAME=sonar_scanner_example # project name from SonarName projet creation page -Dsonar.projectName=XXXX
+set -x -e
 
-# Configuration for SonarCloud
-#SONAR_TOKEN= # access token from SonarCloud projet creation page -Dsonar.login=XXXX: here it is defined in the environment through the CI
-SONAR_PROJECT_KEY=sonarcloud_example_cpp-cmake-linux-otherci # project name from SonarCloud projet creation page -Dsonar.projectKey=XXXX
-SONAR_PROJECT_NAME=sonarcloud_example_cpp-cmake-linux-otherci # project name from SonarCloud projet creation page -Dsonar.projectName=XXXX
-SONAR_ORGANIZATION=sonarcloud # organization name from SonarCloud projet creation page -Dsonar.organization=ZZZZ
+ls -all
+rm -rf $1/dist
+rm -rf $1/build
+#rm -rf $1/nbproject/Makefile-impl.mk
+#rm -rf $1/nbproject/Makefile-default.mk
+#rm -rf $1/nbproject/Makefile-genesis.mk
+#rm -rf $1/nbproject/Makefile-local-default.mk
+#rm -rf $1/nbproject/Makefile-rfariables.mk
+#rm -rf $1/nbproject/Package-default.bash
+#rm -rf $1/build/nbproject !(*.xml)
+ls $1 -all
 
-# Set default to SONAR_HOST_URL in not provided
-SONAR_HOST_URL=${SONAR_HOST_URL:-https://sonarcloud.io}
+#/opt/mplabx/mplab_platform/bin/prjMakefilesGenerator.sh $1@$2 || exit 1
+build-wrapper-linux-x86-64 --out-dir build_wrapper_output_directory make -C $1 CONF=$2 build || exit 4
 
-mkdir $HOME/.sonar
-export SONAR_SCANNER_VERSION=4.7.0.2747
-export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
-
-# download sonar-scanner
-curl -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip 
-unzip -o $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
-export PATH=$SONAR_SCANNER_HOME/bin:$PATH
-export SONAR_SCANNER_OPTS="-server"
-
-# download build-wrapper
-curl -sSLo $HOME/.sonar/build-wrapper-linux-x86.zip https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
-unzip -o $HOME/.sonar/build-wrapper-linux-x86.zip -d $HOME/.sonar/
-export PATH=$HOME/.sonar/build-wrapper-linux-x86:$PATH
-
-# Setup the build system
-rm -rf build
-mkdir build
-cd build
-cmake ..
-cd ..
-
-# Build inside the build-wrapper
-build-wrapper-linux-x86-64 --out-dir build_wrapper_output_directory cmake --build build/ --config Release
-
-# Run sonar scanner (here, arguments are passed through the command line but most of them can be written in the sonar-project.properties file)
-[[ -v SONAR_TOKEN ]] && SONAR_TOKEN_CMD_ARG="-Dsonar.login=${SONAR_TOKEN}"
-[[ -v SONAR_ORGANIZATION ]] && SONAR_ORGANIZATION_CMD_ARG="-Dsonar.organization=${SONAR_ORGANIZATION}"
-[[ -v SONAR_PROJECT_NAME ]] && SONAR_PROJECT_NAME_CMD_ARG="-Dsonar.projectName=${SONAR_PROJECT_NAME}"
-SONAR_OTHER_ARGS="-Dsonar.projectVersion=1.0 -Dsonar.sources=src -Dsonar.cfamily.build-wrapper-output=build_wrapper_output_directory -Dsonar.sourceEncoding=UTF-8"
-sonar-scanner -Dsonar.host.url="${SONAR_HOST_URL}" -Dsonar.projectKey=${SONAR_PROJECT_KEY} ${SONAR_OTHER_ARGS} ${SONAR_PROJECT_NAME_CMD_ARG} ${SONAR_TOKEN_CMD_ARG} ${SONAR_ORGANIZATION_CMD_ARG}
+# Run sonar scanner
+sonar-scanner -Dsonar.organization=dsatsangi -Dsonar.projectKey=dsatsangi_xdmc -Dsonar.sources=. -Dsonar.cfamily.build-wrapper-output=bw-output -Dsonar.host.url=https://sonarcloud.io
