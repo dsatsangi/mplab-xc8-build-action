91535337d9b140c755a6c0bf3b06963ebb7133f2
Dheeraz Satsangi <satsangi.dheeraz@gmail.com>
2022-12-07 08:50:43 +0530

    rem

--- build.sh
+++ build.sh
@@ -19,8 +19,8 @@ rm -rf $1/build
 #rm -rf $1/build/nbproject !(*.xml)
 #ls $1 -all
 
-/opt/mplabx/mplab_platform/bin/prjMakefilesGenerator.sh $1@$2 || exit 1
-build-wrapper-linux-x86-64 --out-dir build_wrapper_output_directory make -C $1 CONF=$2 build || exit 2
+#/opt/mplabx/mplab_platform/bin/prjMakefilesGenerator.sh $1@$2 || exit 1
+#build-wrapper-linux-x86-64 --out-dir build_wrapper_output_directory make -C $1 CONF=$2 build || exit 2
 
 # Run sonar scanner
-sonar-scanner -Dsonar.organization=dsatsangi -Dsonar.projectKey=dsatsangi_xdmc -Dsonar.sources=. -Dsonar.cfamily.build-wrapper-output=bw-output -Dsonar.host.url=https://sonarcloud.io
+#sonar-scanner -Dsonar.organization=dsatsangi -Dsonar.projectKey=dsatsangi_xdmc -Dsonar.sources=. -Dsonar.cfamily.build-wrapper-output=bw-output -Dsonar.host.url=https://sonarcloud.io
