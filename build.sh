96bc299feecf5360c07cf655c9520a72b8d22c0b
Dheeraz Satsangi <satsangi.dheeraz@gmail.com>
2022-12-15 15:47:19 +0530

    other-ci config

--- build.sh
+++ build.sh
@@ -4,19 +4,16 @@ echo "Docker Container Building $1:$2"
 
 echo $PATH
 
-# download build-wrapper
-wget https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
-unzip -o build-wrapper-linux-x86.zip -d /opt/sonar
-rm build-wrapper-linux-x86.zip
+export SONAR_SCANNER_VERSION=4.7.0.2747
+export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
+curl --create-dirs -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip
+unzip -o $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
+export PATH=$SONAR_SCANNER_HOME/bin:$PATH
+export SONAR_SCANNER_OPTS="-server"
 
-export PATH=/opt/sonar/build-wrapper-linux-x86/bin:$PATH
-
-# download sonar-scanner
-wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.7.0.2747-linux.zip
-unzip sonar-scanner-cli-4.7.0.2747-linux.zip -d /opt/sonar
-rm sonar-scanner-cli-4.7.0.2747-linux.zip
-
-export PATH=/opt/sonar/sonar-scanner-cli-4.7.0.2747-linux/bin:$PATH
+curl --create-dirs -sSLo $HOME/.sonar/build-wrapper-linux-x86.zip https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
+unzip -o $HOME/.sonar/build-wrapper-linux-x86.zip -d $HOME/.sonar/
+export PATH=$HOME/.sonar/build-wrapper-linux-x86:$PATH
 
 pwd
 ls -all
