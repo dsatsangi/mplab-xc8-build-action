15bffe6161dcfd7421b7b3d30a7660288beb5ec0
Dheeraz Satsangi <satsangi.dheeraz@gmail.com>
2022-12-15 12:38:16 +0530

    possible

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ ls -all
 rm -rf $1/dist
 rm -rf $1/build
 
-#build-wrapper-linux-x86-64 --out-dir build_wrapper_output_directory make -C $1 CONF=$2 build
-#sonar-scanner --define sonar.cfamily.build-wrapper-output="${{ env.BUILD_WRAPPER_OUT_DIR }}"
+build-wrapper-linux-x86-64 --out-dir build_wrapper_output_directory make -C $1 CONF=$2 build
+sonar-scanner --define sonar.cfamily.build-wrapper-output="${{ env.BUILD_WRAPPER_OUT_DIR }}"
